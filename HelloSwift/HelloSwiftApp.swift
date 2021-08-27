3b0a609d086da12d65c77e8867679b1ab61d6d28
liangjiaxin <liang.jiaxin@mayi888.com>
2021-08-27 16:13:03 +0800

    Initial Commit

---
+++ HelloSwift/HelloSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftApp.swift
+//  HelloSwift
+//
+//  Created by 梁家新 on 2021/8/27.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
